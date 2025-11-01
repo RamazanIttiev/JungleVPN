c002c801c80211278a2efddecca5682cbb16ae4d
rittiev <rittiev@devexperts.com>
2025-11-01 14:43:42 +0000

    Removed unused files

--- src/bot/navigation/core/utils.ts
+++
@@ -1,6 +0,0 @@
-import { RouterLocation } from '@bot/navigation/core/conversations/conversations.types';
-
-export async function navigate(ctx: any, location: RouterLocation) {
-  ctx.session.location = location;
-  await ctx.router.handleUpdate(ctx.update);
-}
