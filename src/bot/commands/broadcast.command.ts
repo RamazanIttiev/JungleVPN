419f52c101912cf8d5881394427cc1a714cadcc4
rittiev <rittiev@devexperts.com>
2025-11-11 13:06:41 +0000

    Broadcast to all users

--- src/bot/commands/broadcast.command.ts
+++ src/bot/commands/broadcast.command.ts
@@ -8,21 +8,23 @@ export class BroadcastCommand {
   constructor(readonly remnaService: RemnaService) {}
 
   register(bot: Bot<BotContext>) {
-    bot.on('message', async (ctx) => {
+    bot.command('message', async (ctx) => {
       const adminId = Number(process.env.TELEGRAM_ADMIN_ID);
-      const fromId = ctx.from.id;
+      const fromId = ctx.from?.id;
       if (fromId !== adminId) return;
 
       const message = ctx.message?.text;
       const users = await this.remnaService.getAllUsers();
 
-      if (!message || message.startsWith('/')) return;
+      const textToSend = message?.split('\n').slice(1).join('\n');
+
+      if (!message || message.startsWith('/start')) return;
+      if (!textToSend || textToSend.startsWith('/start')) return;
 
       for (const { telegramId } of users) {
         if (telegramId === adminId) continue;
         try {
-          console.log(`Sending to ${telegramId}: ${message}`);
-          // await ctx.api.sendMessage(telegramId, message);
+          await ctx.api.sendMessage(telegramId, textToSend);
         } catch (e) {
           console.log(`Failed to send to ${telegramId}:`, e);
         }
